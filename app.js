9aeec1d9cdd0f10b062254af12d8cc519f76c3f2
fmedrano17 <fmedrano1998@gmail.com>
2022-01-21 14:42:03 -0600

    Added document.write to JS file

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
+document.write("<h1>Header</h1>");
+document.write("<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>")
 console.log('Hello! How has your day been?');
