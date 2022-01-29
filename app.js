a007bd9b9d806e5131c2180c1ba7571f960ca235
fmedrano17 <fmedrano1998@gmail.com>
2022-01-28 23:26:14 -0600

    2.4 assignment

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-document.write("<h1>Header</h1>");
-document.write("<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>")
-console.log('Hello! How has your day been?');
+// object variable using const
+const assignment1 = {
+    name: "Fernando Medrano",
+    programName: "Calculator"
+};
+
+// string variable
+let welcome = "Hello! This is a program designed for IMED-2351-001.";
+
+// global variable
+var result;
+
+// variable values (will output to console)
+var number1 = 50;
+var number2 = 30;
+
+{
+    // block variable
+    var number3 = 20;
+
+    // mathematical operation for the variables
+    result = number1 + number2 + number3;
+}
+
+// string variable logged out to console
+console.log(welcome);
+
+// output variables and result to console
+console.log("This ", assignment1.programName, "was designed by ", assignment1.name, ".");
+console.log("The numbers ", number1, ", ", number2, ", and ", number3, " add up to ", result);
