afd9c85d1c18c40f64a93bcf23fa3743773d2d4e
fmedrano17 <fmedrano1998@gmail.com>
2022-02-04 14:55:43 -0600

    module3 update

--- module3/app.js
+++ module3/app.js
@@ -1,9 +1,9 @@
 console.log("Hello! This program gives you the date and time, as well as the due date for this assignment");
 
 const dueDate = {
-    month = "2",
-    date = "4",
-    year = "2022"
+    month: "2",
+    date: "4",
+    year: "2022"
 };
 
 function getDueDate(month, day, year) {
