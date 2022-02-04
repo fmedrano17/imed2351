489b22b16e3338f1e2f7ea9174fec314f81fd5e6
fmedrano17 <fmedrano1998@gmail.com>
2022-02-04 16:04:43 -0600

    added CSS, tweaked html and js

--- module3/app.js
+++ module3/app.js
@@ -1,16 +1,17 @@
-console.log("Hello! This program gives you todays date, as well as the due date for this assignment.");
-
+// Object with multiple properties
 const dueDate = {
     month: "2",
     date: "4",
     year: "2022"
 };
 
+// Gets due date by passing in paramaters, and passes out the due date
 function getDueDate(month, date, year) {
     var dueDateString = (month + "-" + date + "-" + year);
     return dueDateString;
 }
 
+// Gets todays date using methods, passes out todays date
 function todaysDate() {
     const d = new Date();
     let month = d.getMonth()+1;
@@ -20,5 +21,8 @@ function todaysDate() {
     return dateString;
 }
 
-console.log("The current date is: " + todaysDate());
-console.log("The due date is: " + getDueDate(dueDate.month, dueDate.date, dueDate.year));
+//  Pushes an alert with the due date and current date
+function dateAlert() {
+    alert("The current date is: " + todaysDate() +
+          "\nThe due date is: " + getDueDate(dueDate.month, dueDate.date, dueDate.year));
+}
