0834479b6315860bff754215ae546fe1659bceeb
fmedrano17 <fmedrano1998@gmail.com>
2022-02-04 14:51:52 -0600

    Update app.js

--- module3/app.js
+++ module3/app.js
@@ -1,5 +1,14 @@
-function date() {
-    console.log("Todays date is: ")
+console.log("Hello! This program gives you the date and time, as well as the due date for this assignment");
+
+const dueDate = {
+    month = "2",
+    date = "4",
+    year = "2022"
+};
+
+function getDueDate(month, day, year) {
+    let dueDateString = (month + "-" + date + "-" + year);
+    return dueDateString
 }
 
-date();
+console.log("The due date is: " + getDueDate);
